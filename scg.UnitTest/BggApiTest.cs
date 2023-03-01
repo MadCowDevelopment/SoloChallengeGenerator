d8886133ed2f800e44e241162ed1bab6ab07deee
Mihi <devdumbblind@googlemail.com>
2023-03-01 11:06:49 +0800

    Fixed JSON encoding for geeklist item body

--- scg.UnitTest/BggApiTest.cs
+++ scg.UnitTest/BggApiTest.cs
@@ -1,4 +1,5 @@
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 using scg.Services;
 using Xunit;
 
@@ -12,6 +13,13 @@ public class BggApiTest
         var api = new BggApiService();
 
         await api.Login("", "");
-        await api.PostGeeklistItem(249689, 265736, "No nonsense from C#");
+        await api.PostGeeklistItem(249689, 265736, @"[b]Challenge Start:[/b] Now
+[b]Challenge End:[/b] 03/31/2023
+
+[b]Description:[/b]
+Randomized objectives, buildings and specialists.
+
+[b]Details found here:[/b]
+[thread=$THREAD_ID$][/thread]");
     }
 }
